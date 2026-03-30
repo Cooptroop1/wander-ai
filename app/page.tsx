6bfc89ce7b1d8535fb1648aa0458d9c6792cda5b
Cooptroop1 <jcooper4888@aol.co.uk>
2026-03-31 00:54:44 +0100

    Add WelcomePopup component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@
 import dynamic from 'next/dynamic';
 
 const WorldMap = dynamic(() => import('../components/WorldMap'), { ssr: false });
+const WelcomePopup = dynamic(() => import('../components/WelcomePopup'), { ssr: false });
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-zinc-950 text-white">
+      <WelcomePopup />
       <WorldMap />
     </main>
   );
