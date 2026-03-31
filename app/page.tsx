0756662010b4fc4634ac46d91321ac99ca5ca610
Cooptroop1 <jcooper4888@aol.co.uk>
2026-04-01 00:35:26 +0100

    Dynamically import WorldMap and WelcomePopup components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 'use client';
 
-import WorldMap from '../components/WorldMap';
+import dynamic from 'next/dynamic';
+
+const WorldMap = dynamic(() => import('../components/WorldMap'), { ssr: false });
+const WelcomePopup = dynamic(() => import('../components/WelcomePopup'), { ssr: false });
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-zinc-950 text-white">
+      <WelcomePopup />
       <WorldMap />
     </main>
   );
